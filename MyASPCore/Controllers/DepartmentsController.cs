c787609c64baf813ced46e1bba269b05ab9d1bbf
erickkurniawan <erick.kurniawan@gmail.com>
2023-11-21 14:57:22 +0700

    menambahkan crud dari web api

--- MyASPCore/Controllers/DepartmentsController.cs
+++ MyASPCore/Controllers/DepartmentsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyASPCore.Models;
 using MyASPCore.Services;
 using MyASPCore.ViewModels;
 
@@ -39,9 +40,82 @@ namespace MyASPCore.Controllers
             catch (System.Exception ex)
             {
                 ViewData["error"] = ex.Message;
+                return Content(ex.Message);
+            }
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DepartmentCreateViewModel departmentCreateViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
                 return View();
             }
 
+            try
+            {
+                Department department = new Department
+                {
+                    DepartmentName = departmentCreateViewModel.DepartmentName
+                };
+                await _departmentService.Insert(department);
+
+                TempData["pesan"] = $"Data Department {department.DepartmentName} has added";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewData["error"] = $"<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'>&times;</button><strong>Error!</strong>{ex.Message}</div>";
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                var editDepartment = await _departmentService.GetById(id);
+                DepartmentViewModel model = new DepartmentViewModel
+                {
+                    DepartmentID = editDepartment.DepartmentID,
+                    DepartmentName = editDepartment.DepartmentName
+                };
+                return View(model);
+            }
+            catch (System.Exception ex)
+            {
+                return View();
+            }
+        }
+
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public async Task<IActionResult> EditPost(DepartmentViewModel departmentViewModel)
+        {
+            try
+            {
+                var editDepartment = new Department
+                {
+                    DepartmentID = departmentViewModel.DepartmentID,
+                    DepartmentName = departmentViewModel.DepartmentName
+                };
+                await _departmentService.Update(editDepartment);
+
+                TempData["pesan"] = $"Data Department {editDepartment.DepartmentName} has updated";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (System.Exception ex)
+            {
+                ViewData["error"] = $"<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'>&times;</button><strong>Error!</strong>{ex.Message}</div>";
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- MyASPCore/Services/DepartmentService.cs
+++ MyASPCore/Services/DepartmentService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using MyASPCore.Models;
@@ -14,17 +15,33 @@ namespace MyASPCore.Services
         {
             _httpClient = httpClient;
         }
-        public Task Delete(int id)
+        public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Authorization =
+              new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVyaWNrQGdtYWlsLmNvbSIsInJvbGUiOlsiYWRtaW4iLCJmaW5hbmNlIl0sIm5iZiI6MTcwMDU0OTMxNSwiZXhwIjoxNzAwNjM1NzE1LCJpYXQiOjE3MDA1NDkzMTV9.DtaTq3vOmZS9QoJiDraQHutGdnZqZECnQLgDQ0D-IIk");
+            try
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"http://localhost:5001/api/Departments/{id}");
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Status Code: {response.StatusCode} - {response.Content.ToString()}");
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task<IEnumerable<Department>> GetAll()
         {
             List<Department> departments = new List<Department>();
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVyaWNrQGdtYWlsLmNvbSIsInJvbGUiOlsiYWRtaW4iLCJmaW5hbmNlIl0sIm5iZiI6MTcwMDU0OTMxNSwiZXhwIjoxNzAwNjM1NzE1LCJpYXQiOjE3MDA1NDkzMTV9.DtaTq3vOmZS9QoJiDraQHutGdnZqZECnQLgDQ0D-IIk");
             HttpResponseMessage response = await _httpClient.GetAsync("http://localhost:5001/api/Departments");
             if (!response.IsSuccessStatusCode)
                 throw new Exception($"Status Code: {response.StatusCode} - {response.Content.ToString()}");
+
             string content = await response.Content.ReadAsStringAsync();
             departments = JsonSerializer.Deserialize<List<Department>>(content);
             if (departments == null)
@@ -33,19 +50,68 @@ namespace MyASPCore.Services
             return departments;
         }
 
-        public Task<Department> GetById(int id)
+        public async Task<Department> GetById(int id)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVyaWNrQGdtYWlsLmNvbSIsInJvbGUiOlsiYWRtaW4iLCJmaW5hbmNlIl0sIm5iZiI6MTcwMDU0OTMxNSwiZXhwIjoxNzAwNjM1NzE1LCJpYXQiOjE3MDA1NDkzMTV9.DtaTq3vOmZS9QoJiDraQHutGdnZqZECnQLgDQ0D-IIk");
+            HttpResponseMessage response = await _httpClient.GetAsync($"http://localhost:5001/api/Departments/{id}");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"Status Code: {response.StatusCode} - {response.Content.ToString()}");
+            string content = await response.Content.ReadAsStringAsync();
+            var department = JsonSerializer.Deserialize<Department>(content);
+            return department;
         }
 
-        public Task<Department> Insert(Department department)
+        public async Task<Department> Insert(Department department)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVyaWNrQGdtYWlsLmNvbSIsInJvbGUiOlsiYWRtaW4iLCJmaW5hbmNlIl0sIm5iZiI6MTcwMDU0OTMxNSwiZXhwIjoxNzAwNjM1NzE1LCJpYXQiOjE3MDA1NDkzMTV9.DtaTq3vOmZS9QoJiDraQHutGdnZqZECnQLgDQ0D-IIk");
+            string jsonObject = JsonSerializer.Serialize(department);
+            StringContent content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+            try
+            {
+                HttpResponseMessage response =
+                    await _httpClient.PostAsync("http://localhost:5001/api/Departments", content);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Status Code: {response.StatusCode} - {response.Content.ToString()}");
+
+                string outputContent = await response.Content.ReadAsStringAsync();
+                var outputDepartment = JsonSerializer.Deserialize<Department>(outputContent);
+
+                return outputDepartment;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        public Task<Department> Update(Department department)
+        public async Task<Department> Update(Department department)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVyaWNrQGdtYWlsLmNvbSIsInJvbGUiOlsiYWRtaW4iLCJmaW5hbmNlIl0sIm5iZiI6MTcwMDU0OTMxNSwiZXhwIjoxNzAwNjM1NzE1LCJpYXQiOjE3MDA1NDkzMTV9.DtaTq3vOmZS9QoJiDraQHutGdnZqZECnQLgDQ0D-IIk");
+            string jsonObject = JsonSerializer.Serialize(department);
+            StringContent content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+            try
+            {
+                HttpResponseMessage response =
+                    await _httpClient.PutAsync($"http://localhost:5001/api/Departments/{department.DepartmentID}", content);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Status Code: {response.StatusCode} - {response.Content.ToString()}");
+
+                string outputContent = await response.Content.ReadAsStringAsync();
+                var outputDepartment = JsonSerializer.Deserialize<Department>(outputContent);
+
+                return outputDepartment;
+
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
